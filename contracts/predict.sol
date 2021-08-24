cf889266df2464f6dfe5649cdb90fd3046ab6ec4
Greg Plumbly <greg.plumbly@gmail.com>
2021-08-24 11:17:32 +0100

    few changes after call with Rajesh

--- contracts/predict.sol
+++ contracts/predict.sol
@@ -2,25 +2,30 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-contract Storage {
+contract FootballPrediction {
     address public manager;
     address[] public players;
     address[] public winners;
     
+    // struct with game and date
+    // fixtures array of structs
+    // mappings
+    
     constructor() {
         manager = msg.sender;
     }
-
-    function createFixture(string memory _game, uint256 _matchdate) public restricted { // _game format = 'MNCVARS' (Man City V Arsenal)
-    } 
-
-    function makePrediction() public {
+    
+    function createFixtures (string memory _game, uint256 _matchdate) public restricted returns (uint fixtureId) {
     }
 
-
+    function updateResults (fixtureId, team1_score, team2_score) public restricted { //struct for multiple scores?
+    }
+    
     function calculateWinners()  public restricted {
     }
 
+    function makePrediction() public { // user would also send transfer for lottery ticket with predictions
+    }
 
     function withdrawWinnings() public {
     }
