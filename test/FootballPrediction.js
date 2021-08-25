d08f0b4f950eb473dd48d03937fb185f7aa6af94
Greg Plumbly <greg.plumbly@gmail.com>
2021-08-25 09:45:04 +0100

    Add test that manager is contract deployer.

---
+++ test/FootballPrediction.js
@@ -0,0 +1,21 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+let owner;
+let addr1;
+
+describe("Football Prediction", () => {
+  let FootballPrediction, footballPrediction;
+
+  beforeEach(async () => {
+    FootballPrediction = await ethers.getContractFactory("FootballPrediction");
+    footballPrediction = await FootballPrediction.deploy();
+    [owner, addr1] = await ethers.getSigners();
+  });
+
+  it("Should set the manager as the contract deployer", async function () {
+    console.log(await footballPrediction.manager());
+    console.log(owner.address);
+    expect(await footballPrediction.manager()).to.equal(owner.address);
+  });
+});
